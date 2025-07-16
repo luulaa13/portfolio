cc9d74394363bc6a0922cecad94b8330646d2918
luulaa13 <luciagarciagarcia.lgg@gmail.com>
2025-07-16 12:20:46 +0200

    Update Portfolio.jsx

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -1,8 +1,17 @@
-export default function Portfolio() {
-    return (
-      <div className="portfolio-container">
-          <Navbar />
-      </div>
-    )
-  }
-  
+import Navbar from '../components/Navbar'
+import React from 'react'
+
+const Portfolio = () => {
+  return (
+    <div>
+      <Navbar />
+      {/* Aquí van tus secciones futuras */}
+      <section id="proyectos" style={{ paddingTop: '100px' }}>Proyectos</section>
+      <section id="sobre-mi">Sobre mí</section>
+      <section id="newsletter">Newsletter</section>
+      <section id="contacto">Contacto</section>
+    </div>
+  )
+}
+
+export default Portfolio
