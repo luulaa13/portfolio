a450fb204d287a32438b3893fde12cd206a486b5
luulaa13 <luciagarciagarcia.lgg@gmail.com>
2025-07-16 09:52:03 +0200

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -33,8 +33,9 @@ export default function App() {
       </Canvas>
 
       <div className="info-container">
-        <h1 className="info-title">Hola, soy Lucía</h1>
-        <p className="subtitle">Software Developer & UX/UI Designer</p>
+       <p class="intro">Hi! I'm</p>
+        <h1>Lucia Garcia</h1>
+        <p class="subtitle">Software Developer & UX/UI Designer</p>
         <Link to="/portfolio" className="portfolio-button">
           Ver portfolio
         </Link>
