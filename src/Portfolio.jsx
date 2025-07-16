95bbc7c48bd223433d8344edf69f16e86c1baab1
luulaa13 <luciagarciagarcia.lgg@gmail.com>
2025-07-16 12:25:02 +0200

    Update Portfolio.jsx

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -5,11 +5,7 @@ const Portfolio = () => {
   return (
     <div>
       <Navbar />
-      {/* Aquí van tus secciones futuras */}
-      <section id="proyectos" style={{ paddingTop: '100px' }}>Proyectos</section>
-      <section id="sobre-mi">Sobre mí</section>
-      <section id="newsletter">Newsletter</section>
-      <section id="contacto">Contacto</section>
+
     </div>
   )
 }
