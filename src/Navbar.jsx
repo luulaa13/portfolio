2256145518b4bb5383919ab8589b8af4806a9bb0
Lucia Garcia Garcia <lucia@macbook-air-lula-1.home>
2025-07-20 10:21:43 +0200

    Inicio

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,19 +1,9 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import '../src/style/Navbar.css'
 import logo from '../src/assets/logo.png';
 
 const Navbar = () => {
-  const [darkMode, setDarkMode] = useState(false)
   const [menuOpen, setMenuOpen] = useState(false)
-
-  useEffect(() => {
-    const root = document.documentElement
-    if (darkMode) {
-      root.classList.add('dark-mode')
-    } else {
-      root.classList.remove('dark-mode')
-    }
-  }, [darkMode])
 
   const toggleMenu = () => setMenuOpen(!menuOpen)
   const closeMenu = () => setMenuOpen(false)
@@ -25,25 +15,10 @@ const Navbar = () => {
           <img src={logo} alt="Logo" className="logo-image" />
         </div>
 
-        <div className="theme-switch">
-          <label className="switch">
-            <input
-              type="checkbox"
-              checked={darkMode}
-              onChange={() => setDarkMode(!darkMode)}
-            />
-            <div className="track">
-              <div className={`switch-icon ${!darkMode ? 'active' : ''}`}>🌞</div>
-              <div className={`switch-icon ${darkMode ? 'active' : ''}`}>🌙</div>
-            </div>
-          </label>
-        </div>
-
-
+        {/* Eliminado el switch de tema */}
 
         <div className="navbar__menu" onClick={toggleMenu}>
           <div className="hamburger">
-            
             <span></span>
             <span></span>
             <span></span>
@@ -53,18 +28,19 @@ const Navbar = () => {
 
       {/* Cortina con bordes redondeados */}
       <div className={`dropdown-curtain ${menuOpen ? 'open' : ''}`}>
-      <button className="close-button" onClick={closeMenu}>✕</button>
+        <button className="close-button" onClick={closeMenu}>✕</button>
         <ul>
-        <li><a href="#inicio" onClick={closeMenu}>Inicio</a></li>
-        <li><a href="#about" onClick={closeMenu}>About</a></li>
-        <li><a href="#proyectos" onClick={closeMenu}>Proyectos</a></li>
-        <li><a href="#newsletter" onClick={closeMenu}>Newsletter</a></li>
-        <li><a href="#contacto" onClick={closeMenu}>Contacto</a></li>
+          <li><a href="#inicio" onClick={closeMenu}>Inicio</a></li>
+          <li><a href="#about" onClick={closeMenu}>About</a></li>
+          <li><a href="#proyectos" onClick={closeMenu}>Proyectos</a></li>
+          <li><a href="#newsletter" onClick={closeMenu}>Newsletter</a></li>
+          <li><a href="#contacto" onClick={closeMenu}>Contacto</a></li>
         </ul>
       </div>
+
       {menuOpen && (
-  <div className="backdrop" onClick={closeMenu}></div>
-)}
+        <div className="backdrop" onClick={closeMenu}></div>
+      )}
     </>
   )
 }
