b81f04e66f781e7da898c4fcac4a33b4889ebb02
luulaa13 <luciagarciagarcia.lgg@gmail.com>
2025-07-22 12:36:31 +0200

    Update Portfolio.jsx

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -14,63 +14,21 @@ import BackstageGif from '../src/assets/backstage.gif';
 
 const Portfolio = () => {
 
-  const buttonsRef = useRef(null);
-
-  useEffect(() => {
-    gsap.fromTo(
-      buttonsRef.current.children,
-      { opacity: 0, y: 40 },
-      {
-        opacity: 1,
-        y: 0,
-        duration: 1,
-        stagger: 0.2,
-        ease: 'power3.out',
-      }
-    );
-  }, []);
-  
 
   return (
     <div className="portfolio-container">
       <Navbar />
       <section id="inicio" className="section">
-    
-        <div className="hero-text">
-          <h1>HOLA, SOY LUCIA</h1>
-          <p className="hero-subtitle">
-          Diseño, desarrollo y estrategia digital para crear experiencias únicas que conectan y transforman
-          </p>
-        </div>
-
-        <div className="hero-buttons" ref={buttonsRef}>
-          <a href="#about" className="hero-btn secondary">Conoceme</a>
-          <a href="#proyectos" className="hero-btn primary">Explorar proyectos</a>
-        </div>
-     
-        <div className="scroll-down-arrow">
-        <svg width="24" height="24" viewBox="0 0 24 24">
-          <path d="M12 5v14m0 0l-6-6m6 6l6-6" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round"/>
-        </svg>
-      </div>
-      
       </section>
       <MovingBanner/>
 
       <section id="about" className="section">
-         <div className="sticker-wrapper">
-            <img src={Sticker} className="sticker" />
-          </div>
       </section>
 
       <section id="proyectos" className="section">
-       
       </section>
 
-     
-
       <section id="newsletter" className="section">
-        
       </section>
 
       <section id="contacto" className="contacto-section">
