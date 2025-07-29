1b2255fd6f7bf9f9bb85ba58dc574ecdb733974f
Lucia Garcia Garcia <lucia@macbook-air-lula-1.home>
2025-07-29 19:37:01 +0200

    Inicio

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -1,9 +1,14 @@
 import Navbar from './Navbar.jsx'
 import Newsletter from './Newsletter.jsx'
 import AboutMe from './AboutMe.jsx'
-import Sticker from './Sticker2.png'
-import Ticket from './ticket.png'
-import StringTie from './image.png'
+import Sticker from '../src/assets/Sticker2.png'
+import Ticket from '../src/assets/ticket.png'
+import StringTie from '../src/assets/image.png'
+import Camara from '../src/assets/camara.png'
+import Star from '../src/assets/giphy.gif'
+import Post from '../src/assets/post.png'
+import QR from '../src/assets/qr.png'
+import Exclamation from '../src/assets/exclamation.gif'
 import '../src/style/Portfolio.css' // Importamos el CSS externo
 import React from 'react'
 import  MovingBanner from './MovingBanner.jsx'
@@ -11,22 +16,57 @@ import Linkedin from '../src/assets/linkedin.svg';
 import Dribble from '../src/assets/dribble.svg';
 import Github from '../src/assets/github.svg';
 import BackstageGif from '../src/assets/backstage.gif'; 
+import { useEffect, useRef, useState } from 'react';
 
 
 const Portfolio = () => {
+  const [showStickers, setShowStickers] = useState(false);
+  const wrapperRef = useRef(null);
 
+  useEffect(() => {
+    const wrapper = wrapperRef.current; // ✅ copia local
+  
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setShowStickers(true);
+        }
+      },
+      { threshold: 0.5 }
+    );
+  
+    if (wrapper) {
+      observer.observe(wrapper);
+    }
+  
+    return () => {
+      if (wrapper) {
+        observer.unobserve(wrapper);
+      }
+    };
+  }, []);
+  
 
   return (
     <div className="portfolio-container">
       <Navbar />
       <section id="inicio" className="section">
+
       </section>
       <MovingBanner/>
 
-      <section id="about" className="section">
-          <div className="stickers-wrapper">
+      <section id="about" className="section" >
+      <div
+        className={`stickers-wrapper ${showStickers ? "show" : ""}`}
+        ref={wrapperRef}
+      >
           <img src={Sticker} alt="Sticker decorativo" className="sticker main-sticker" />
           <img src={Ticket} alt="Skills ticket" className="sticker ticket" />
+          <img src={Exclamation} alt="Exclamacion icono" className="sticker exclamation" />
+          <img src={Camara} alt="Foto camara vintage" className="sticker camara" />
+          <img src={Post} alt="About post" className="sticker post" />
+          <img src={QR} alt="qr" className="sticker qr" />
+          <img src={Star} alt="Gif decoracion" className="sticker gif" />
         </div>
       </section>
 
